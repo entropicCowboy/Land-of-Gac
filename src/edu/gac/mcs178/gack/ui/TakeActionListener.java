481ae070ce01c36dfc3f41a4c8a726e20b9e93bf
entropicCowboy <118836121+entropicCowboy@users.noreply.github.com>
2025-02-18 10:12:27 -0600

    Initial commit

---
+++ src/edu/gac/mcs178/gack/ui/TakeActionListener.java
@@ -0,0 +1,62 @@
+package edu.gac.mcs178.gack.ui;
+
+import edu.gac.mcs178.gack.domain.Person;
+import edu.gac.mcs178.gack.domain.Thing;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.JComboBox;
+
+public class TakeActionListener implements ActionListener {
+	
+	private static final Thing INTSRUCTIONS = new Thing("Take ...");
+	
+	private GraphicalUserInterface gui;
+	private Person player;
+	private JComboBox takeJComboBox;
+	private boolean enabled;
+	private List<Thing> things;
+
+	public TakeActionListener(GraphicalUserInterface gui, Person player, JComboBox takeJComboBox) {
+		super();
+		this.gui = gui;
+		this.player = player;
+		this.takeJComboBox = takeJComboBox;
+		this.enabled = true;
+		things = player.otherThingsAtSamePlace();
+		takeJComboBox.addItem(INTSRUCTIONS);
+		for (Thing thing : things) {
+			takeJComboBox.addItem(thing);
+		}
+	}
+	
+	public void setEnabled(boolean b) {
+		enabled = b;
+	}
+	
+	public void updateJComboBox() {
+		takeJComboBox.removeAllItems();
+		things = player.otherThingsAtSamePlace();
+		takeJComboBox.addItem(INTSRUCTIONS);
+		for (Thing thing : things) {
+			takeJComboBox.addItem(thing);
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (enabled) {
+			if (!player.getIsFrog()) {
+				Thing item = (Thing) takeJComboBox.getSelectedItem();
+				if (!item.equals(INTSRUCTIONS)) {
+					gui.displayMessage("\n>>> Take " + item);
+					player.take(item);
+					gui.playTurn();
+				}
+			}
+			else {	// Player is a frog and cannot pick up items
+				gui.displayMessage("player says -- It's not easy being green");
+			}
+		}
+	}
+}
